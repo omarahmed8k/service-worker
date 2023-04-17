eca9cc5a899c3e686baf838cfbefcfdace035be8
Omar Abdelrahman <omarahmed8k@gmail.com>
2023-04-17 07:33:11 +0200

    update

--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./sw.js')
         .then(function (registration) {
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            console.log('[Service Worker] registration successful with scope: ', registration.scope);
         })
         .catch(function (err) {
-            console.log('ServiceWorker registration failed: ', err);
+            console.log('[Service Worker] registration failed: ', err);
         });
 }
 
